aab58e5ee5297784c10b6b715e141ba2b2afd487
abinashramkumar <abinashramkumar@gmail.com>
2024-10-14 22:27:56 +0530

    first commit

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main{
+    public static void main(String[] args) {
+        int abinash = 23;
+        int raj = 54;
+        int sum = abinash+raj;
+        System.out.println(sum);
+    }}
